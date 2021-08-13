3d5d97fbd70ce2782bc90197b6428f5ce31fa364
keshibat <kensukeshibata@gmail.com>
2021-08-13 16:57:30 +0900

    api GET /planets getAllPlanets

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 
+const planetRouter = require('./routes/planets/planets.router');
+
 const app = express();
 app.use(express.json());
+app.use(planetsRouter);
 
 module.exports = app;
---
+++ src/routes/planets/planets.router.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const {
+  getAllPlanets
+} = require('./planets.controller');
+
+const planetsRouter = express.Router();
+
+planetsRouter.get('/planets', getAllPlanets);
+
+module.exports = planetsRouter;
+
+
+
+
+
