d0145761d10bb032233be542683c0164d20a3d70
keshibat <kensukeshibata@gmail.com>
2021-09-18 11:54:22 +0900

    connecting to mongoDB

--- src/server.js
+++ src/server.js
@@ -1,15 +1,28 @@
- const http = require('http');
+const http = require('http');
+const mongoose = require('mongoose');
 
- const app = require('./app');
 
- const { loadPlanetsData } = require('./models/planets.models')
+const app = require('./app');
+const { loadPlanetsData } = require('./models/planets.models');
 
 const PORT = process.env.PORT || 8000;
 
+const MONGO_URI = 'mongodb+srv://admin:Secret1.0@nasacluster.d8thx.mongodb.net/nasa?retryWrites=true&w=majority';
+
 const server = http.createServer(app);
 
+mongoose.connection.once('open', () => {
+  console.log('MongoDB connection is ready!')
+});
+
+mongoose.connection.on('error', (err) => {
+  console.error(err);
+});
 
 async function StartServer() {
+  await mongoose.connect(MONGO_URI, {
+    useNewUrlParser: true,
+  });
   await loadPlanetsData();
   server.listen(PORT, () => {
     console.log(`Listening on port ${PORT}...`);
