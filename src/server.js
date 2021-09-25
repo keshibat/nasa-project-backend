277d6de136b08dd6da0235ac0162764e68340dab
keshibat <kensukeshibata@gmail.com>
2021-09-25 13:42:01 +0900

    added .env and rotate use and password

--- src/server.js
+++ src/server.js
@@ -1,34 +1,24 @@
 const http = require('http');
-const mongoose = require('mongoose');
 
+require('dotenv').config();
 
 const app = require('./app');
+const { mongoConnect } = require('./services/mongo');
 const { loadPlanetsData } = require('./models/planets.model');
 const { loadLaunchData } = require('./models/launches.model');
 
 const PORT = process.env.PORT || 8000;
 
-const MONGO_URI = 'mongodb+srv://admin:Secret1.0@nasacluster.d8thx.mongodb.net/nasa?retryWrites=true&w=majority';
-
 const server = http.createServer(app);
 
-mongoose.connection.once('open', () => {
-  console.log('MongoDB connection is ready!')
-});
-
-mongoose.connection.on('error', (err) => {
-  console.error(err);
-});
-
 async function StartServer() {
-  await mongoose.connect(MONGO_URI, {
-    useNewUrlParser: true,
-  });
+  await mongoConnect();
   await loadPlanetsData();
+  await loadLaunchData();
+
   server.listen(PORT, () => {
     console.log(`Listening on port ${PORT}...`);
   });
-  await loadLaunchData();
 }
 
 StartServer();
--- src/services/mongo.js
+++ src/services/mongo.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 
-const MONGO_URL = 'mongodb+srv://admin:Secret1.0@nasacluster.d8thx.mongodb.net/nasa?retryWrites=true&w=majority'
-
+const MONGO_URL = process.env.MONGO_URL;
 
 mongoose.connection.once('open', () => {
   console.log('MongoDB connection ready!')
